124fcd1da49bb485c272e8e7917361b3c4bdddef
lee <iamlyg9667@kookmin.ac.kr>
2025-07-01 23:41:53 +0900

    클래스형 컴포넌트 구조

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 /* @jsx createElement */
-import { createElement, render } from './react';
+import { createElement, render, Component } from "./react";
 
-function Title(props) {
-  return <h1>{ props.children }</h1>;
+class Title extends Component {
+  render() {
+    return <h1>{ this.props.children }</h1>
+  }
 }
 
 function Item(props) {
   return <li style={`color:${props.color}`}>{props.children}</li>
 }
 
-const vdom = <p>
-  <Title label="React">React 정말 잘 만들기</Title>
+const App = () => <p>
+  <Title label="React">React 정말 클래스 컴포넌트 잘 만들기</Title>
   <ul>
     <Item color="red">첫 번째 아이템</Item>
     <Item color="green">두 번째 아이템</Item>
@@ -18,4 +20,4 @@ const vdom = <p>
   </ul>
 </p>
 
-render(vdom, document.querySelector('#root'));
+render(<App />, document.querySelector('#root'));
--- src/react.js
+++ src/react.js
@@ -1,4 +1,10 @@
 
+export class Component {
+  constructor(props) {
+    this.props = props;
+  }
+}
+
 export function createDOM(node) {
   if (typeof node === 'string') {
     return document.createTextNode(node);
@@ -16,23 +22,37 @@ export function createDOM(node) {
   return element;
 }
 
+function makeProps(props, children) {
+  return {
+    ...props,
+    children: children.length === 1 ? children[0] : children,
+  };
+}
+
 export function createElement(tag, props, ...children) {
   props = props || {};
   
   if (typeof tag === 'function') {
-    if (children.length > 0) {
+    if (tag.prototype instanceof Component) {
+      const instance = new tag(makeProps(props, children));
+      return instance.render();
+    } else {
+      if (children.length > 0) {
       return tag({
         ...props,
         children: children.length === 1 ? children[0] : children,
       })
     } else {
       return tag(props);
-    }
-  } else {
-    return { tag, props, children };
+    } 
+   
   }
+} else {
+    return { tag, props, children };
+}
 }
 
 export function render(vdom, container) {
   container.appendChild(createDOM(vdom));  
 }
+
